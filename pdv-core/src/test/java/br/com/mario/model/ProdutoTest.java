e03a34c0dfca84d940c51bec5731272143d80f54
Mário Russo <mario.russo93@hotmail.com>
2025-04-14 13:51:22 -0300

    Test: Movimentação de estoque saida e erro

--- pdv-core/src/test/java/br/com/mario/model/ProdutoTest.java
+++ pdv-core/src/test/java/br/com/mario/model/ProdutoTest.java
@@ -1,19 +1,39 @@
 package br.com.mario.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.mario.model.util.EstoqueException;
+
 @ExtendWith(MockitoExtension.class)
 public class ProdutoTest {
 
-    Produto produto = new Produto();
+    Produto produto = ProdutoFactory.produtoFactory();
 
     @Test
-    void testa_criacao_da_classe(){
-        produto.setNome("café");
-        assertEquals("café", produto.getNome());;
+    void testa_criacao_da_classe() {
+        assertEquals(ProdutoFactory.nome, produto.getNome());
+    }
+
+    @Test
+    @DisplayName("Retorna erro quando a quantidade é mais baixa que o estoque!")
+    void retorna_erro_produto_estoque_baixo() {
+        produto.setQuantidadeEstoque(5);
+
+        assertThrows(EstoqueException.class, () -> {
+            produto.atualizaEStoque(6);
+        });
+    }
+
+    @Test
+    void atualiza_estoque() {
+        int valorEsperado = 1;
+        produto.atualizaEStoque(4);
+        assertEquals(valorEsperado, produto.getQuantidadeEstoque());
     }
 }
