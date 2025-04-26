e732913c668e36e262b6d36c17482b56dce55a17
Mário Russo <mario.russo93@hotmail.com>
2025-04-26 16:36:11 -0300

    Feat: criação do banco de dados

---
+++ pdv-core/src/main/java/br/com/mario/dataBese/SQLiteConnection.java
@@ -0,0 +1,33 @@
+package br.com.mario.dataBese;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteConnection {
+
+    private final static String url = "jdbc:sqlite:pdv.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url);
+    }
+
+    public static void criarTabelas() {
+        String sqlProdutos = "CREATE TABLE IF NOT EXISTS produtos (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "codigo INTEGER NOT NULL, " +
+                "nome TEXT NOT NULL, " +
+                "descricao TEXT NOT NULL, " +
+                "preco REAL NOT NULL, " +
+                "quantidadeEstoque INTEGER NOT NULL)";
+
+        try (Connection conn = getConnection();
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sqlProdutos);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
